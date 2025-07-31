ccf1f64efe2d6735f30cc4f58a5233e075345f3b
Nida Tanveer <nidtan786@gmail.com>
2025-07-31 16:12:02 +0000

    updated changed to monthly campaign finance table

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,17 +1,18 @@
 SELECT
 EXTRACT(MONTH from date_date) as by_month,
-nb_orders,
-operational_margin,
-ads_clicks
-ads_cost,
-ads_impression,
-ads_margin,
-purchase_cost,
-shipping_fee,
-log_cost,
-ship_cost,
-quantity,
-revenue,
-margin
+SUM(nb_orders)AS nb_orders,
+SUM(operational_margin) AS operational_margin,
+SUM(ads_clicks) AS ads_clicks,
+SUM(ads_cost) AS ads_cost,
+SUM(ads_impression) AS ads_impression,
+SUM(ads_margin) AS ads_margin,
+SUM(purchase_cost) AS purchase_cost,
+SUM(shipping_fee) AS shipping_fee,
+SUM(log_cost) AS log_cost,
+SUM(ship_cost) AS ship_cost,
+SUM(quantity) AS quantity,
+SUM(revenue) AS revenue,
+SUM(margin) AS margin
 FROM {{ref('finance_campaigns_day')}}
+GROUP BY by_month
 ORDER BY by_month 
